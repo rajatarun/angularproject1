04022327253e3a0bd57ef5c2ff80f01cb64d8c86
rajatarun <traja1@binghamton.edu>
2016-01-19 16:47:27 -0500

    popup changes

--- scripts/FoodApp.js
+++ scripts/FoodApp.js
@@ -1,9 +1,34 @@
 (function(){
 	var foodapp = angular.module("FoodApp",[]);
+	
+	
+	/*  foodapp.factory('ndbId', function($rootScope) {
+  		var sharedService = {};
+    
+    sharedService.ndbid = 09427;
+
+    sharedService.setndbId = function(msg) {
+    	this.ndbid=msg;
+    	alert(this.ndbid);
+        
+    };
+
+    return {"value":sharedService.ndbid};
+  		
+	});*/
+	
 	foodapp.controller("BodyController",function($scope,$http){
-		$http.get("http://api.nal.usda.gov/ndb/reports/?ndbno=01267&type=f&format=json&api_key=DEMO_KEY").success(function(response){
+		var url = "http://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&api_key=DEMO_KEY";
+		$http.get(url).success(function(response){
 			$scope.data=response;
 		});
+
+	});
+	foodapp.controller("FoodBodyController",function($scope,$http){
+		$http.get(" http://api.nal.usda.gov/ndb/reports/?ndbno="+$scope.$parent.ndbno+''+"&type=f&format=json&api_key=DEMO_KEY").success(function(response){
+			$scope.data=response;
+		});
+
 	});
 
 })();
